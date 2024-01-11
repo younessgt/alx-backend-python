beef1995e34043138dfcb7b5e2166283ee252edc
younessgt <youness.merzak123@gmail.com>
2024-01-11 12:09:38 +0100

    task0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""script contain add function"""
+
+
+def add(a: float, b: float) -> float:
+    """ Function to add two floats"""
+    return a + b
---
+++ README.md
@@ -0,0 +1,1 @@
+Python Back-end
