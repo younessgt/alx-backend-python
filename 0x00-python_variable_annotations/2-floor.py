16c54c6b2d143f9f2566ff8ddb7f84052997e7a1
younessgt <youness.merzak123@gmail.com>
2024-01-11 12:27:22 +0100

    task2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" script contain floor function"""
+
+
+def floor(n: float) -> int:
+    """ function takes a float n as argument and
+    returns the floor of the float"""
+    return int(n)
