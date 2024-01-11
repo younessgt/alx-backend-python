e4f75e4e1378f25e29c429a2679ba81798850f82
younessgt <youness.merzak123@gmail.com>
2024-01-11 12:14:24 +0100

    task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" script contain concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function that concatenate two strings """
+    return str1 + str2
