38a2a7cae6fd8aa16ddb976bbb5cd9f4ec79b9da
younessgt <youness.merzak123@gmail.com>
2024-01-11 13:09:13 +0100

    task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""script contain sum_list function """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """function that takes a list input_list of floats as argument
+    and returns their sum as a float."""
+    return sum(input_list)
