7d22ca76ea78bd5edba05c47c8dd83333db9dacc
younessgt <youness.merzak123@gmail.com>
2024-01-15 15:08:50 +0100

    fixing type annotated requirement on task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ function that takes in an integer argument and
     wait for a random delay between 0 and max_delay seconds
     and eventually returns it"""
