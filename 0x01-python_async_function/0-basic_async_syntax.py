dbd499f5aab04d01409f1a6063b16c8c3849e1ff
younessgt <youness.merzak123@gmail.com>
2024-01-15 15:07:01 +0100

    task0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""script conatin wait_random function """
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ function that takes in an integer argument and
+    wait for a random delay between 0 and max_delay seconds
+    and eventually returns it"""
+
+    randnm_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(randnm_delay)
+    return randnm_delay
