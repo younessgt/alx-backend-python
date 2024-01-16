3621c3f16d772da089cf27d7c7ec08f1aa40cc03
younessgt <youness.merzak123@gmail.com>
2024-01-16 13:42:06 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ asynchronus generator that yield a number for each iteration"""
     for _ in range(10):
         await asyncio.sleep(1)
