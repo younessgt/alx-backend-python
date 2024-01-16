21c12b4665e33d6e6227c819c9eb60aa206613ee
younessgt <youness.merzak123@gmail.com>
2024-01-16 15:34:32 +0100

    task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """function doc needed"""
     start_time = time.perf_counter()
 
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
