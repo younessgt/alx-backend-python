237eb7cc5499c01612dc445ab21ded72e83b0f49
younessgt <youness.merzak123@gmail.com>
2024-02-03 23:38:49 +0100

    task3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,12 +2,12 @@
 """ script using unitest to test the function "access_nested_map"""
 import unittest
 from unittest import mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    '''Class usin functions to test access_nested_map function.'''
+    '''Class using functions to test access_nested_map function.'''
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
@@ -39,3 +39,32 @@ class TestGetJson(unittest.TestCase):
         with mock.patch('requests.get') as mock_get:
             mock_get.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """" class using functions to test memoize function."""
+
+    def test_memoize(self):
+        """Method to test memoize function."""
+
+        class TestClass:
+            """class to define."""
+
+            def a_method(self):
+                """method to define"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """method to define"""
+                return self.a_method()
+
+        with mock.patch.object(TestClass,
+                               'a_method',
+                               return_value=42) as mock_patch:
+            test_obj = TestClass()
+            ret = test_obj.a_property
+            ret = test_obj.a_property
+
+            self.assertEqual(ret, 42)
+            mock_patch.assert_called_once()
